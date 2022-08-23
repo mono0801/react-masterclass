af4b1f6e627c5a6a9dedb02080cf2caca9dfa655
sellen <austin0801@naver.com>
2022-08-23 20:28:26 +0900

    Theme
    Styled-components에서
    
    Theme을 사용하여
    Darkmode, Lightmode로 전환하는 법
    
    나머지 50%는
    local Estate Management에서 공부

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Animation from "./Animation";
+import Theme from "./Theme";
+import { ThemeProvider } from "styled-components";
+
+// textColor, backgroundColor와 같이 이름이 같아야한다
+const darkTheme = {
+    textColor: "white",
+    backgroundColor: "#111",
+};
+
+const lightTheme = {
+    textColor: "#111",
+    backgroundColor: "white",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
     <React.StrictMode>
-        <App />
-        <Animation />
+        <ThemeProvider theme={darkTheme}>
+            {/* <App />
+            <Animation /> */}
+            <Theme />
+        </ThemeProvider>
     </React.StrictMode>
 );
