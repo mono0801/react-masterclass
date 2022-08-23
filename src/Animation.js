52afc8a9d19553bd03f9007a3e25818f8253b99e
sellen <austin0801@naver.com>
2022-08-23 18:13:29 +0900

    Update Animation.js
    style-component에서
    
    컴포넌트 내의 태그를 설정
    Pseudo Selector (hover 같은) 사용법

--- src/Animation.js
+++ src/Animation.js
@@ -10,11 +10,11 @@ const rotationAnimation = keyframes`
         border-radius: 0px;
     }
     50%{
-        transform: rotate(360deg);
+        transform: rotate(180deg);
         border-radius: 100px;
     }
     100%{
-        transform: rotate(0deg);
+        transform: rotate(360deg);
         border-radius: 0px;
     }
 `;
@@ -23,13 +23,32 @@ const Box = styled.div`
     height: 200px;
     width: 200px;
     background-color: wheat;
-    animation: ${rotationAnimation} 1s linear infinite;
+    animation: ${rotationAnimation} 1.2s linear infinite;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    // Box 컴포넌트 안에 있는 span을 직접 설정할 수 있다
+    span {
+        font-size: 500%;
+
+        // span:hover => &:hover 대체 가능
+        &:hover {
+            font-size: 700%;
+        }
+        &:active {
+            opacity: 0;
+        }
+    }
 `;
 
 function Animation() {
     return (
         <Wrapper>
-            <Box></Box>
+            <Box>
+                <span>🤔</span>
+            </Box>
         </Wrapper>
     );
 }
