62b2e3a44a5f91ae057fc7af4e65babbf3737b6d
sellen <austin0801@naver.com>
2022-08-23 18:04:11 +0900

    Create Animation.js
    styled-components에서
    
    animation 효과를 설정하는 법

---
+++ src/Animation.js
@@ -0,0 +1,37 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+`;
+
+const rotationAnimation = keyframes`
+    0%{
+        transform: rotate(0deg);
+        border-radius: 0px;
+    }
+    50%{
+        transform: rotate(360deg);
+        border-radius: 100px;
+    }
+    100%{
+        transform: rotate(0deg);
+        border-radius: 0px;
+    }
+`;
+
+const Box = styled.div`
+    height: 200px;
+    width: 200px;
+    background-color: wheat;
+    animation: ${rotationAnimation} 1s linear infinite;
+`;
+
+function Animation() {
+    return (
+        <Wrapper>
+            <Box></Box>
+        </Wrapper>
+    );
+}
+
+export default Animation;
